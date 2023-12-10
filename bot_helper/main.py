4937025eeaca54d7bc62afa02e9d05bdeb4c960c
Vadym Kuzmenko <notarum@gmail.com>
2023-12-10 17:11:34 +0000

    Added error handling

--- bot_helper/main.py
+++ bot_helper/main.py
@@ -10,31 +10,57 @@ MENU:
 """
 
 
+def input_error(func):
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except (ValueError,TypeError) as e:
+            print(f"{e}")
+    return wrapper
+
 def show_menu():
     return MENU
-
 
 def parse_input(user_input):
     cmd, *args = user_input.split()
     cmd = cmd.strip().lower()
     return cmd, *args
 
-
+@input_error
 def add_contact(args, contacts):
+    if len(args) != 2:
+        raise ValueError("Add command expect 2 arguments name and phone. Try again")
     name, phone = args
+    if len(name) < 3:
+        raise ValueError("Name should be more then 3 symbols. Try again")
+    if len(phone) < 10:
+        raise ValueError("Phone should be more not less 10 symbols. Try again")
+    if phone.isdigit() == False:
+        raise TypeError("Phone number should include only digits")
+    
     contacts[name] = phone
     return "Contact added."
 
-
+@input_error
 def change_contact(args, contacts):
+    if len(args) != 2:
+        raise ValueError("Change command expect 2 arguments name and phone. Try again")
+    
     name, phone = args
+
+    if len(phone) < 10:
+        raise ValueError("Phone should be more not less 10 symbols. Try again")
+    
+    if phone.isdigit() == False:
+        raise TypeError("Phone number should include only digits")
+    
     if name in contacts:
         contacts.update({name: phone})
         return "Contact updated."
     else:
         return "Contact not found."
-
-
+    
+@input_error
 def show_phone(args, contacts):
     (name,) = args
     if name in contacts:
@@ -42,13 +68,18 @@ def show_phone(args, contacts):
     else:
         return "Contact not found."
 
-
+@input_error
 def show_all(contacts):
     all_records = ""
-
+    if len(contacts) == 0:
+        raise ValueError("There are not contacts in list. Add some new one.")
     for key, value in contacts.items():
         all_records += f"{key}: {value}\n"
     return all_records
+
+@input_error
+def show_menu():
+    print(MENU)
 
 def main():
     contacts = {}
