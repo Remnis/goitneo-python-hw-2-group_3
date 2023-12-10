fe70c840e25d85f986760a6a2edc06974930bc4b
Vadym Kuzmenko <notarum@gmail.com>
2023-12-10 18:06:50 +0000

    Added pass to clasess

--- bot_helper_oop/main.py
+++ bot_helper_oop/main.py
@@ -8,27 +8,27 @@ class Field:
         return str(self.value)
 
 class Name(Field):
-    # реалізація класу
+    pass
 
 class Phone(Field):
-    # реалізація класу
+   pass
 
 class Record:
     def __init__(self, name):
         self.name = Name(name)
         self.phones = []
 
-    # реалізація класу
+    
 
     def __str__(self):
         return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
 
 class AddressBook(UserDict):
-    # реалізація класу
+   pass
 
 
 
-    # Створення нової адресної книги
+# Створення нової адресної книги
 book = AddressBook()
 
 # Створення запису для John
